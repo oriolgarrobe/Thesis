d6b0ca80ab007fd959220e161a0abdd0081ee2e2
oriolgarrobe <oriol.garrobe@gmail.com>
2021-03-30 20:35:16 +0200

    variance sample + simulate tails

--- helper_functions.py
+++ helper_functions.py
@@ -86,7 +86,7 @@ def standarize(y,pct,pct_lower):
     y_std = y_std.flatten()
     return y_std,len_y,y
     
-
+from random import random
 def simulation(n_points, base_model, base_class, dist):
     """
     Function that simulates observations for bla bla. 
